2fd1037106f5cf7e89e03e983623154ead56005a
cemch24 <codemch@yahoo.com>
2023-03-19 21:30:10 -0400

    second commit

---
+++ Watchlist/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+
+
+def index(request): 
+    return render(request,"Watchlist/index.html")
+
+def currentlyWatching(request): 
+    context = {}
+    return render(request,"Watchlist/currentlyWatching.html")
+
+def recommendations(request): 
+    context = {}
+    return render(request,"Watchlist/recommendations.html")
+
+def login(request): 
+    context = {}
+    return render(request,"Watchlist/login.html")
+
+
